eef04e091b9934a70e039a6b5bab4352d16d36ce
Avinash Pathak (AVPATHAK) <AVPATHAK@nettools1-eastus2.phx.gbl>
2022-06-07 07:46:21 +0000

    updated protos as per latest changes

--- setup.sh
+++ setup.sh
@@ -12,21 +12,22 @@ setup_gnoi_repo() {
 	git clone https://github.com/openconfig/gnoi.git 
 	pushd gnoi > /dev/null
 	git branch -d xr_gnoi_client_phase_4 >& /dev/null
-	git checkout -b xr_gnoi_client_phase_4 0b5aea31df40c60251a246ba9047cd256488036b > /dev/null
+	#git checkout -b xr_gnoi_client_phase_4 0b5aea31df40c60251a246ba9047cd256488036b > /dev/null
+	git checkout -b xr_gnoi_client_phase_4 2e1ce73b56028fa34e860896fa935c21d6446058 > /dev/null
 	popd > /dev/null
 	popd > /dev/null
    fi
 }
 
 # Setting python 2.7 virtualenv and all grpc packages
-if [[ ! -f xr-venv/bin/activate ]]; then
+#if [[ ! -f xr-venv3/bin/activate ]]; then
 	#virtualenv ./xr-venv
-        /router/bin/python3 -m venv ./xr-venv3
+        python3 -m virtualenv ./xr-venv3
 	source xr-venv3/bin/activate > /dev/null
         pip install --upgrade pip
 	pip install grpcio > /dev/null
-	pip install grpcio-tools > /dev/null
-fi
+	#pip install grpcio-tools > /dev/null
+#fi
 
 # Uncomment the next line if need to generate pb2:
 setup_gnoi_repo
